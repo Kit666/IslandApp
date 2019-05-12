774a12265d4b3d4426512a2d56a9365307919d55
Kit666 <kit666@qq.com>
2019-05-12 17:38:40 +0800

    Island8-1

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -13,8 +13,12 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //数据更新setData
     classic.getLatest( (res) => {
-      
+      // console.log(res);
+      this.setData({
+        classicData: res
+      })
     })
   },
 
